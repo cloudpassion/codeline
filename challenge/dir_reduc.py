d8ba21ef3618e866f8b5b427f5041e5738ff51a9
clp <2052756+cloudpassion@users.noreply.github.com>
2020-04-11 01:58:56 +0300

    re to py

---
+++ challenge/dir_reduc.py
@@ -0,0 +1,26 @@
+import help
+import time
+
+test = help.test()
+
+def dirReduc(arr):
+
+    _ret = []
+    b = ''.join(arr)
+    for i in range(0,len(arr)):
+        #b = b.replace('SOUTHSOUTH','').replace('NORTHNORTH','').replace('WESTWEST','').replace('EASTEAST','')
+        b = b.replace('SOUTHNORTH','').replace('NORTHSOUTH','').replace('WESTEAST','').replace('EASTWEST','')
+
+    for z in [ 'SOUTH', 'NORTH', 'WEST', 'EAST' ]:
+        b = b.replace(z, z+' ')
+
+    for zz in b.split(' '):
+        if zz != '':
+            _ret.append(zz.replace(' ',''))
+
+    return _ret
+
+a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+test.assert_equals(dirReduc(a), ['WEST'])
+u=["NORTH", "WEST", "SOUTH", "EAST"]
+test.assert_equals(dirReduc(u), ["NORTH", "WEST", "SOUTH", "EAST"])
